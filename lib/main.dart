aa52062897a1c1a5a89f8c9631aa3eb7fed0e562
Joe Miguel <miguerdz1023@gmail.com>
2022-04-06 09:42:30 -0500

    Ventana de trabajos realizados y no realizados
    Para el tecnico

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -10,26 +10,19 @@ class MyApp extends StatelessWidget {
 
   static const String _title = 'GpoLias';
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+
+       primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text(_title),
+
         ),
         body: const MyStatefulWidget(
           title: _title,),
@@ -37,18 +30,48 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(''),
+
+            automaticallyImplyLeading: false,
+            backgroundColor: Color(0xff5808e5),
+            bottom: TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(text: 'DOGS', icon: Icon(Icons.favorite)),
+                Tab(text: 'CATS', icon: Icon(Icons.music_note)),
+                //Tab(text: 'BIRDS', icon: Icon(Icons.search)),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(child: Text('DOGS')),
+              Center(child: Text('CATS')),
+              //Center(child: Text('BIRDS')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
 
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -57,6 +80,7 @@ class MyStatefulWidget extends StatefulWidget {
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
 
@@ -112,8 +136,11 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                 child: ElevatedButton(
                   child: const Text('Login'),
                   onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
+                    //print(nameController.text);
+                    //print(passwordController.text);
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => SecondScreen()),
+                    );
                   },
                 )
             ),
@@ -136,3 +163,5 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
         ));
   }
 }
+
+
